4f7450a3d30b23f6cc7d5abf6393a31c1c206c50
Su5eD <su5ed@outlook.com>
2023-08-11 22:45:24 +0200

    Disable ItemBlockRenderTypes client check past client setup
    Fixes #35

--- src/mod/java/dev/su5ed/sinytra/connector/mod/ConnectorMod.java
+++ src/mod/java/dev/su5ed/sinytra/connector/mod/ConnectorMod.java
@@ -7,11 +7,17 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod("connectormod")
 public class ConnectorMod {
+    private static boolean clientLoadComplete;
+
+    public static boolean clientLoadComplete() {
+        return clientLoadComplete;
+    }
 
     public ConnectorMod() {
         Throwable loadingException = ConnectorEarlyLoader.getLoadingException();
@@ -20,6 +26,7 @@ public class ConnectorMod {
         }
 
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(ConnectorMod::onClientSetup);
         ModList modList = ModList.get();
         if (FMLLoader.getDist().isClient() && modList.isLoaded("fabric_rendering_fluids_v1")) {
             bus.addListener(FluidHandlerCompat::onClientSetup);
@@ -28,5 +35,9 @@ public class ConnectorMod {
         if (modList.isLoaded("fabric_object_builder_api_v1")) {
             bus.addListener(EventPriority.HIGHEST, LazyEntityAttributes::addMissingAttributes);
         }
+    }
+
+    private static void onClientSetup(FMLClientSetupEvent event) {
+        clientLoadComplete = true;
     }
 }

0c41b24b2c697a40b613875f1304ba0be12e9a64
melontini <104443436+melontini@users.noreply.github.com>
2023-08-26 16:28:07 +0700

    Recipe book (#113)
    * Patch the recipe book.
    
    * Fixed a possible issue.
    
    * Added missing `@Mutable`
    
    * Style changes.
    
    * Moved mixins to `recipebook` subpackage.

---
+++ src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/recipebook/RecipeBookCategoriesAccessor.java
@@ -0,0 +1,19 @@
+package dev.su5ed.sinytra.connector.mod.mixin.recipebook;
+
+import net.minecraft.client.RecipeBookCategories;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(RecipeBookCategories.class)
+public interface RecipeBookCategoriesAccessor {
+
+    @Accessor("AGGREGATE_CATEGORIES")
+    @Mutable
+    static void setAGGREGATE_CATEGORIES(Map<RecipeBookCategories, List<RecipeBookCategories>> map) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/recipebook/RecipeBookManagerMixin.java
@@ -0,0 +1,38 @@
+package dev.su5ed.sinytra.connector.mod.mixin.recipebook;
+
+import net.minecraft.client.RecipeBookCategories;
+import net.minecraftforge.client.RecipeBookManager;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.*;
+
+@Mixin(value = RecipeBookManager.class, remap = false)
+public class RecipeBookManagerMixin {
+    @Mutable
+    @Shadow
+    @Final
+    private static Map<RecipeBookCategories, List<RecipeBookCategories>> AGGREGATE_CATEGORIES;
+
+    @Inject(at = @At("TAIL"), method = "init", remap = false)
+    private static void init(CallbackInfo ci) {
+        //Make lists mutable.
+        AGGREGATE_CATEGORIES.replaceAll((category, categories) -> new ArrayList<>(categories));
+
+        //Since we replaced RecipeBookCategories.AGGREGATE_CATEGORIES,
+        //we need to add entries that might have been added by fabric mods.
+        RecipeBookCategories.AGGREGATE_CATEGORIES.forEach((category, categories) -> {
+            List<RecipeBookCategories> eventCategories = AGGREGATE_CATEGORIES.computeIfAbsent(category, categories1 -> new ArrayList<>());
+            //We need to make categories mutable, so we can remove them. Just in case mods return an immutable list.
+            List<RecipeBookCategories> mutableCategories = new ArrayList<>(categories);
+            mutableCategories.removeIf(eventCategories::contains);
+            //We, unfortunately, can't easily respect insertion order here.
+            eventCategories.addAll(mutableCategories);
+        });
+
+        //Set the reference to the new map.
+        RecipeBookCategoriesAccessor.setAGGREGATE_CATEGORIES(AGGREGATE_CATEGORIES);
+    }
+}

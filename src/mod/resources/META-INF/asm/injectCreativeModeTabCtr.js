5cd20acbedb323837bc91db181c56b0dfde55259
Su5eD <su5ed@outlook.com>
2023-08-28 16:19:26 +0200

    Fix injected CreativeModeTab constructor bridge

--- src/mod/resources/META-INF/asm/injectCreativeModeTabCtr.js
+++ src/mod/resources/META-INF/asm/injectCreativeModeTabCtr.js
@@ -11,23 +11,44 @@ function initializeCoreMod() {
             'transformer': function (node) {
                 var method = node.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "(Lnet/minecraft/world/item/CreativeModeTab$Row;ILnet/minecraft/world/item/CreativeModeTab$Type;Lnet/minecraft/network/chat/Component;Ljava/util/function/Supplier;Lnet/minecraft/world/item/CreativeModeTab$DisplayItemsGenerator;)V", null, null);
                 method.visitCode();
+                
                 method.visitVarInsn(Opcodes.ALOAD, 0);
+                // row
                 method.visitVarInsn(Opcodes.ALOAD, 1);
+                // column
                 method.visitVarInsn(Opcodes.ILOAD, 2);
+                // type
                 method.visitVarInsn(Opcodes.ALOAD, 3);
+                // displayName
                 method.visitVarInsn(Opcodes.ALOAD, 4);
+                // iconGenerator
                 method.visitVarInsn(Opcodes.ALOAD, 5);
+                // displayItemsGenerator
                 method.visitVarInsn(Opcodes.ALOAD, 6);
-                method.visitTypeInsn(Opcodes.NEW, "net/minecraft/resources/ResourceLocation"); // backgroundLocation
+                // backgroundLocation
+                method.visitTypeInsn(Opcodes.NEW, "net/minecraft/resources/ResourceLocation");
                 method.visitInsn(Opcodes.DUP);
                 method.visitLdcInsn("textures/gui/container/creative_inventory/tab_items.png");
                 method.visitMethodInsn(Opcodes.INVOKESPECIAL, "net/minecraft/resources/ResourceLocation", "<init>", "(Ljava/lang/String;)V");
-                method.visitInsn(Opcodes.ICONST_0); // hasSearchBar
-                method.visitLdcInsn(89); // searchBarWidth
+                // hasSearchBar
+                method.visitInsn(Opcodes.ICONST_0);
+                // searchBarWidth
+                method.visitLdcInsn(89);
                 method.visitFieldInsn(Opcodes.GETSTATIC, "net/minecraft/world/item/CreativeModeTab$Builder", "CREATIVE_INVENTORY_TABS_IMAGE", "Lnet/minecraft/resources/ResourceLocation;"); // tabsImage
-                method.visitLdcInsn(4210752); // labelColor
-                method.visitLdcInsn(-2130706433); // slotColor
-                method.visitMethodInsn(Opcodes.INVOKESPECIAL, "net/minecraft/world/item/CreativeModeTab", "<init>", "(Lnet/minecraft/world/item/CreativeModeTab$Row;ILnet/minecraft/world/item/CreativeModeTab$Type;Lnet/minecraft/network/chat/Component;Ljava/util/function/Supplier;Lnet/minecraft/world/item/CreativeModeTab$DisplayItemsGenerator;Lnet/minecraft/resources/ResourceLocation;ZILnet/minecraft/resources/ResourceLocation;II)V", false);
+                // labelColor
+                method.visitLdcInsn(4210752);
+                // slotColor
+                method.visitLdcInsn(-2130706433);
+                // tabsBefore
+                method.visitTypeInsn(Opcodes.NEW, "java/util/ArrayList")
+                method.visitInsn(Opcodes.DUP)
+                method.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/ArrayList", "<init>", "()V")
+                // tabsAfter
+                method.visitTypeInsn(Opcodes.NEW, "java/util/ArrayList")
+                method.visitInsn(Opcodes.DUP)
+                method.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/ArrayList", "<init>", "()V")
+                // invoke ctr
+                method.visitMethodInsn(Opcodes.INVOKESPECIAL, "net/minecraft/world/item/CreativeModeTab", "<init>", "(Lnet/minecraft/world/item/CreativeModeTab$Row;ILnet/minecraft/world/item/CreativeModeTab$Type;Lnet/minecraft/network/chat/Component;Ljava/util/function/Supplier;Lnet/minecraft/world/item/CreativeModeTab$DisplayItemsGenerator;Lnet/minecraft/resources/ResourceLocation;ZILnet/minecraft/resources/ResourceLocation;IILjava/util/List;Ljava/util/List;)V", false);
                 method.visitInsn(Opcodes.RETURN);
                 method.visitEnd();
 

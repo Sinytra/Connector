c4b048714600df7b86f2c1ee20cbf5edafddbb9f
Su5eD <su5ed@outlook.com>
2023-08-29 18:01:11 +0200

    Put runtime_enum_extender launch plugin before fabric mixin
    Fixes an issue where a mixin made the enum $VALUES field mutable, crashing runtime_enum_extender

--- src/main/java/dev/su5ed/sinytra/connector/service/ConnectorLoaderService.java
+++ src/main/java/dev/su5ed/sinytra/connector/service/ConnectorLoaderService.java
@@ -44,6 +44,8 @@ public class ConnectorLoaderService implements ITransformationService {
             LinkedHashMap<String, ILaunchPluginService> sortedPlugins = new LinkedHashMap<>();
             // Mixin must come first
             sortedPlugins.put("mixin", plugins.remove("mixin"));
+            // Runtime Enum extender will fail if a mixin makes $VALUES mutable first, so it must come before us as well
+            sortedPlugins.put("runtime_enum_extender", plugins.remove("runtime_enum_extender"));
             // Our plugins come after mixin
             injectPlugins.forEach(plugin -> sortedPlugins.put(plugin.name(), plugin));
             // The rest goes to the end

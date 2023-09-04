ea934d059dc92ac482d1da20e9eff9feaa808d94
Su5eD <su5ed@outlook.com>
2023-09-04 15:46:05 +0200

    Support retrieval of untransformed bytecode in mixin plugin
    Related #164

--- src/main/java/dev/su5ed/sinytra/connector/service/ConnectorMixinLaunchPlugin.java
+++ src/main/java/dev/su5ed/sinytra/connector/service/ConnectorMixinLaunchPlugin.java
@@ -1,11 +1,18 @@
 package dev.su5ed.sinytra.connector.service;
 
+import com.google.common.io.Resources;
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.launch.MixinLaunchPlugin;
 import org.spongepowered.asm.launch.Phases;
+import org.spongepowered.asm.transformers.MixinClassReader;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.EnumSet;
+
+import static cpw.mods.modlauncher.api.LamdbaExceptionUtils.uncheck;
 
 public class ConnectorMixinLaunchPlugin extends MixinLaunchPlugin {
     public static final String NAME = "connector_mixin_plugin";
@@ -26,5 +33,24 @@ public class ConnectorMixinLaunchPlugin extends MixinLaunchPlugin {
     @Override
     public boolean processClass(Phase phase, ClassNode classNode, Type classType, String reason) {
         return !reason.equals(ConnectorMixinLaunchPlugin.NAME) && super.processClass(phase, classNode, classType, reason);
+    }
+
+    @Override
+    public ClassNode getClassNode(String name, boolean runTransformers) throws ClassNotFoundException, IOException {
+        // Support retrieval of untransformed bytecode
+        if (!runTransformers) {
+            String internalName = name.replace('.', '/');
+            String pathToClass = internalName + ".class";
+            URL url = Thread.currentThread().getContextClassLoader().getResource(pathToClass);
+            byte[] classBytes = uncheck(() -> Resources.asByteSource(url).read());
+            if (classBytes != null && classBytes.length != 0) {
+                ClassNode classNode = new ClassNode();
+                String canonicalName = name.replace('/', '.');
+                ClassReader classReader = new MixinClassReader(classBytes, canonicalName);
+                classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
+                return classNode;
+            }
+        }
+        return super.getClassNode(name, runTransformers);
     }
 }

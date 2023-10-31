253627976079d0b829c4f583a7a86035a73495c1
Su5eD <su5ed@outlook.com>
2023-11-01 00:28:02 +0100

    Fix hud mixin priority
    Fixes #448

--- src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/hud/GuiMixin.java
+++ src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/hud/GuiMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Gui.class, priority = 9999)
+@Mixin(value = Gui.class, priority = 200)
 public abstract class GuiMixin implements GuiExtensions {
 
     @Override

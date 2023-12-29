d37585be1fdec32d360d9279355ee42276348c49
Waterpicker <Waterpickerenternity@gmail.com>
2023-12-29 17:02:34 -0600

    updated to work 47.2.16 forge

--- src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/registries/ForgeRegistryMixin.java
+++ src/mod/java/dev/su5ed/sinytra/connector/mod/mixin/registries/ForgeRegistryMixin.java
@@ -5,9 +5,12 @@ import com.mojang.serialization.Lifecycle;
 import dev.su5ed.sinytra.connector.mod.ConnectorLoader;
 import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.GameData;
 import net.minecraftforge.registries.IForgeRegistry;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,7 +29,7 @@ public abstract class ForgeRegistryMixin<V> implements IForgeRegistry<V> {
     private BiMap<Object, V> owners;
 
     // Mixin AP complained about not finding the target method, so we use @Desc instead of a string
-    @Inject(target = @Desc(value = "getDelegateOrThrow", args = Object.class, ret = Holder.Reference.class), at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "getDelegateOrThrow(Ljava/lang/Object;)Lnet/minecraft/core/Holder$Reference;", at = @At("HEAD"), cancellable = true, remap = false)
     private void getDelegateOrThrow(V value, CallbackInfoReturnable<Holder.Reference<V>> cir) {
         if (!ConnectorLoader.hasFinishedLoading()) {
             cir.setReturnValue(getDelegate(value).orElseGet(() -> {
@@ -37,6 +40,38 @@ public abstract class ForgeRegistryMixin<V> implements IForgeRegistry<V> {
                 } catch (IllegalStateException e) {
                     // Fallback if the registry does not support intrusive holders
                     return Holder.Reference.createIntrusive(registry.holderOwner(), value);
+                }
+            }));
+        }
+    }
+
+    @Inject(method = "getDelegateOrThrow(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/core/Holder$Reference;", at = @At("HEAD"), cancellable = true, remap = false)
+    private void getDelegateOrThrow(ResourceLocation value, CallbackInfoReturnable<Holder.Reference<V>> cir) {
+        if (!ConnectorLoader.hasFinishedLoading()) {
+            cir.setReturnValue(getDelegate(value).orElseGet(() -> {
+                MappedRegistry<V> registry = GameData.getWrapper(getRegistryKey(), Lifecycle.stable());
+                try {
+                    // Create intrusive holder on the registry to bind the key later
+                    return registry.createIntrusiveHolder(registry.get(value));
+                } catch (IllegalStateException e) {
+                    // Fallback if the registry does not support intrusive holders
+                    return Holder.Reference.createIntrusive(registry.holderOwner(), registry.get(value));
+                }
+            }));
+        }
+    }
+
+    @Inject(method = "getDelegateOrThrow(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/core/Holder$Reference;", at = @At("HEAD"), cancellable = true, remap = false)
+    private void getDelegateOrThrow(ResourceKey<V> value, CallbackInfoReturnable<Holder.Reference<V>> cir) {
+        if (!ConnectorLoader.hasFinishedLoading()) {
+            cir.setReturnValue(getDelegate(value).orElseGet(() -> {
+                MappedRegistry<V> registry = GameData.getWrapper(getRegistryKey(), Lifecycle.stable());
+                try {
+                    // Create intrusive holder on the registry to bind the key later
+                    return registry.createIntrusiveHolder(registry.get(value));
+                } catch (IllegalStateException e) {
+                    // Fallback if the registry does not support intrusive holders
+                    return Holder.Reference.createIntrusive(registry.holderOwner(), registry.get(value));
                 }
             }));
         }

91b161f8b89a9ac2e8cd1d575ffc06b0f37dfcb9
Su5eD <su5ed@outlook.com>
2024-06-06 19:08:12 +0200

    Add datagen entrypoint hook
    Fixes #1128

--- src/mod/java/org/sinytra/connector/mod/mixin/boot/DatagenModLoaderMixin.java
+++ src/mod/java/org/sinytra/connector/mod/mixin/boot/DatagenModLoaderMixin.java
@@ -1,15 +1,15 @@
 package org.sinytra.connector.mod.mixin.boot;
 
+import net.minecraftforge.data.loading.DatagenModLoader;
 import org.sinytra.connector.mod.ConnectorLoader;
-import net.minecraft.server.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Main.class)
-public abstract class ServerMainMixin {
-    @Inject(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/server/loading/ServerModLoader;load()V"), remap = false)
+@Mixin(value = DatagenModLoader.class, remap = false)
+public class DatagenModLoaderMixin {
+    @Inject(method = "begin", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/ModLoader;gatherAndInitializeMods(Lnet/minecraftforge/fml/ModWorkManager$DrivenExecutor;Ljava/util/concurrent/Executor;Ljava/lang/Runnable;)V"))
     private static void earlyInit(CallbackInfo ci) {
         ConnectorLoader.load();
     }

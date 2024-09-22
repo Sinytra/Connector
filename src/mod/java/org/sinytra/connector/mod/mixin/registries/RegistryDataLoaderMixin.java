b3ab92a0a2b908e9df1c70735c8b2df4ec5e564d
dima_dencep <gagarkin.dima2008@gmail.com>
2024-09-22 22:20:03 +0700

    Fix loading on servers (#1419)
    * Fix loading on servers
    
    * Fix running without FFAPI, bump NF
    
    ---------
    
    Co-authored-by: Su5eD <su5ed@outlook.com>

--- src/mod/java/org/sinytra/connector/mod/mixin/registries/RegistryDataLoaderMixin.java
+++ src/mod/java/org/sinytra/connector/mod/mixin/registries/RegistryDataLoaderMixin.java
@@ -19,7 +19,7 @@ public abstract class RegistryDataLoaderMixin {
     // https://github.com/quiqueck/WorldWeaver/blob/8861dbf39c85cdafbaf2caab1783d11c26d78f44/wover-core-api/src/main/java/org/betterx/wover/core/mixin/registry/RegistryDataLoaderMixin.java#L28
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void postInitLast(CallbackInfo ci) {
-        if (RegistryDataLoader.WORLDGEN_REGISTRIES.size() != DataPackRegistriesHooks.getDataPackRegistries().size()) {
+        if (DataPackRegistriesHooks.getDataPackRegistries() != null && RegistryDataLoader.WORLDGEN_REGISTRIES.size() != DataPackRegistriesHooks.getDataPackRegistries().size()) {
             ConnectorMod.LOGGER.info("Detected changes in WORLDGEN_REGISTRIES, updating NeoForge references");
             List<RegistryDataLoader.RegistryData<?>> list = new ArrayList<>(RegistryDataLoader.WORLDGEN_REGISTRIES); 
             DataPackRegistriesHooksAccessor.set_DATA_PACK_REGISTRIES(list);

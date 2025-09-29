5c4684cfd289d521d75b17d36bbc98472a5df880
Su5eD <su5ed@outlook.com>
2025-09-29 21:36:46 +0200

    Fix transformer lib not published to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -291,20 +291,16 @@ publishing {
             from(components["java"])
         }
     }
-}
 
-allprojects {
-    extensions.findByType<PublishingExtension>()?.apply {
-        repositories {
-            val env = System.getenv()
-            if (env["MAVEN_URL"] != null) {
-                repositories.maven {
-                    url = uri(env["MAVEN_URL"] as String)
-                    if (env["MAVEN_USERNAME"] != null) {
-                        credentials {
-                            username = env["MAVEN_USERNAME"]
-                            password = env["MAVEN_PASSWORD"]
-                        }
+    repositories {
+        val env = System.getenv()
+        if (env["MAVEN_URL"] != null) {
+            repositories.maven {
+                url = uri(env["MAVEN_URL"] as String)
+                if (env["MAVEN_USERNAME"] != null) {
+                    credentials {
+                        username = env["MAVEN_USERNAME"]
+                        password = env["MAVEN_PASSWORD"]
                     }
                 }
             }
--- transformer/build.gradle.kts
+++ transformer/build.gradle.kts
@@ -103,6 +103,21 @@ publishing {
             from(components["shadow"])
         }
     }
+
+    repositories {
+        val env = System.getenv()
+        if (env["MAVEN_URL"] != null) {
+            repositories.maven {
+                url = uri(env["MAVEN_URL"] as String)
+                if (env["MAVEN_USERNAME"] != null) {
+                    credentials {
+                        username = env["MAVEN_USERNAME"]
+                        password = env["MAVEN_PASSWORD"]
+                    }
+                }
+            }
+        }
+    }
 }
 
 class Log4JConfigTransformer(private val resourcesDir: File?) : ResourceTransformer {

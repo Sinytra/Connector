eb614b632cf7cf0a848b79015248868b3a5a396e
Su5eD <su5ed@outlook.com>
2026-02-11 21:19:34 +0100

    Bump Adapter to 2.0.27
    Start beta 14
    
    Fix #2109

--- src/main/java/org/sinytra/connector/util/ConnectorUtil.java
+++ src/main/java/org/sinytra/connector/util/ConnectorUtil.java
@@ -39,7 +39,6 @@ public final class ConnectorUtil {
     public static final String FABRIC_MOD_JSON = "fabric.mod.json";
     public static final String MODS_TOML = "META-INF/neoforge.mods.toml";
     public static final String CONNECTOR_MARKER = "connector_transformed";
-    public static final String NEOFORGE_MODID = "neoforge";
     public static final Path CONNECTOR_FOLDER = FMLPaths.MODSDIR.get().resolve(".connector");
     public static final String CONNECTOR_MODID = "connector";
     public static final String CONNECTOR_ISSUE_TRACKER_URL = "https://github.com/Sinytra/Connector/issues";
